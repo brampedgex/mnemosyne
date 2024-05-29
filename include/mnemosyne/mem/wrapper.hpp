c5bb62609302b83c1cc3b8a1862e447bce3f7606
brampedgex <70824773+brampedgex@users.noreply.github.com>
2024-05-29 12:36:18 -0700

    add proper requires constraints to object wrapper

--- include/mnemosyne/mem/wrapper.hpp
+++ include/mnemosyne/mem/wrapper.hpp
@@ -11,21 +11,25 @@ namespace mnem {
     /// Tag used to prevent the initialization of a wrap<T> object.
     constexpr no_construct_t no_construct;
 
+    template <class T>
+    struct wrapper_default_destroy {
+        static void destroy(T* obj) requires requires { obj->~T(); } {
+            obj->~T();
+        }
+    };
+
     /// Controls the constructor behavior of wrap<T> objects.
     template <class T>
-    struct wrapper_traits {
-        static void construct(std::byte* buffer, auto&&... args) {
+    struct wrapper_traits : wrapper_default_destroy<T> {
+        static void construct(std::byte* buffer, auto&&... args) requires requires { new(buffer) T(std::forward<decltype(args)>(args)...); } {
             new(buffer) T(std::forward<decltype(args)>(args)...);
-        }
-
-        static void destroy(T* obj) {
-            obj->~T();
         }
     };
 
     /// General-purpose wrapper for using custom constructors/destructors.
     /// Prevents devirtualization and gives control over constructors/destructors with the wrapper_traits class.
     template <class T, class Traits = wrapper_traits<T>>
+    requires requires(T* obj) { Traits::destroy(obj); }
     class wrap {
     public:
         explicit wrap(no_construct_t) {}
@@ -34,25 +38,39 @@ namespace mnem {
             Traits::destroy(this->get());
         }
 
-        wrap() {
+        wrap() = delete;
+        wrap() requires requires(std::byte* buf) { Traits::construct(buf); } {
             Traits::construct(this->buffer());
         }
 
+        wrap(const wrap<T, Traits>&) = delete;
+        wrap(const wrap<T, Traits>& copy) requires requires(std::byte* buf, const T& obj) { Traits::construct(buf, obj); } {
+            Traits::construct(this->buffer(), *copy);
+        }
+
+        wrap(wrap<T, Traits>&&) = delete;
+        wrap(wrap<T, Traits>&& move) requires requires(std::byte* buf, T&& obj) { Traits::construct(buf, std::move(obj)); } {
+            Traits::construct(this->buffer(), *std::move(move));
+        }
+
         template <class Traits2>
+        requires requires(std::byte* buf, const T& obj) { Traits::construct(buf, obj); }
         wrap(const wrap<T, Traits2>& copy) {
             Traits::construct(this->buffer(), *copy);
         }
 
         template <class Traits2>
+        requires requires(std::byte* buf, T&& obj) { Traits::construct(buf, std::move(obj)); }
         wrap(wrap<T, Traits2>&& move) noexcept(false) {
             Traits::construct(this->buffer(), *std::move(move));
         }
 
-        explicit wrap(std::in_place_t, auto&&... args) {
+        explicit wrap(std::in_place_t, auto&&... args) requires requires { Traits::construct(this->buffer(), std::forward<decltype(args)>(args)...); } {
             Traits::construct(this->buffer(), std::forward<decltype(args)>(args)...);
         }
 
-        void reset(auto&&... args) {
+        /// Destroys the existing underlying value and constructs a new value in-place.
+        void emplace(auto&&... args) requires requires { Traits::construct(this->buffer(), std::forward<decltype(args)>(args)...); } {
             Traits::destroy(this->get());
             Traits::construct(this->buffer(), std::forward<decltype(args)>(args)...);
         }
